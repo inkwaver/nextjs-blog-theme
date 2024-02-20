113e24e685e753979dca070eb22512e576dcdaeb
Narek Chilingaryan <chilingar3@gmail.com>
2024-02-20 15:44:51 +0400

    mooving theme switcher

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,19 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
+import ThemeSwitcher from './ThemeSwitcher.js';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
+      <ThemeSwitcher />
+
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
+      <Image
+  src="/logo.jpg"
+  width={1200}
+  height={600}
+  alt="Image description"
+/>
         <Link href="/">
           <a>{name} </a>
         </Link>
