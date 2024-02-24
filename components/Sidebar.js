877807a295cc0cbb743557bc96129fc7a4d03343
Narek Chilingaryan <chilingar3@gmail.com>
2024-02-24 22:37:17 +0400

    sidebar layout

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,41 +4,43 @@ import { useRouter } from 'next/router';
 import ThemeSwitcher from './ThemeSwitcher.js';
 
 export default function Sidebar({ name }) {
-  const router = useRouter();
+    const router = useRouter();
 
-  // Function to determine if a link is active
-  const isActiveLink = (href) => {
-    return router.pathname === href;
-  };
+    // Function to determine if a link is active
+    const isActiveLink = (href) => {
+        return router.pathname === href;
+    };
 
-  return (
-    <aside className="main">
-      <ThemeSwitcher />
+    return (
+        <aside className="main">
+            <div className='sideabar-inner'>
+                <ThemeSwitcher />
 
-      <nav>
-        <ul>
-          
-          <li>
-            <Link
+                <nav>
+                    <ul>
+                        <li>
+                            <Link
 
-              href="/case-study"
-            >
-              <a className={isActiveLink('/case-study') ? 'active' : ''}>Case-Study</a>
-            </Link>
-          </li>
+                                href="/case-study"
+                            >
+                                <a className={isActiveLink('/case-study') ? 'active' : ''}>Case-Study</a>
+                            </Link>
+                        </li>
 
-          <li>
-            <Link
+                        <li>
+                            <Link
 
-              href="/dev-projects"
-            >
-              <a className={isActiveLink('/dev-projects') ? 'active' : ''}>Dev Projects</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    
+                                href="/dev-projects"
+                            >
+                                <a className={isActiveLink('/dev-projects') ? 'active' : ''}>Dev Projects</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 
-    </aside>
-  );
+
+
+        </aside>
+    );
 }
