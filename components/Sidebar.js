200f79c59a3d0cc2461a181e785173e046e71129
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-07-18 14:31:39 +0400

    Order in sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,52 +1,45 @@
-// Sidebar.js
 import React from 'react';
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 import Links from './Links';
-import { useRouter } from 'next/router';  // Import the useRouter hook
-const Sidebar = ({ devProjects,designProjects,caseStudy }) => {
-    const router = useRouter();
 
-    const isActiveLink = (href) => {
-        return router.pathname === href || router.asPath === href;
-    };
+const Sidebar = ({ devProjects, designProjects, caseStudy }) => {
+  const router = useRouter();
 
-    return (
-        <aside className="main">
-            <div className='sideabar-inner'>
+  const isActiveLink = (href) => {
+    return router.pathname === href || router.asPath === href;
+  };
 
-                <nav>
-                    <ul>
-                        <li>
-                            <h3>Case Study</h3>
-                            <span>Real Estate</span>
-                            <ul className='articles'>
-                                {caseStudy.map((post) => (
-                                    <li key={post.filePath}>
-                                        {/* eslint-disable */}
-                                        <Link className={`${isActiveLink(`/posts/${post.filePath.replace(/\.mdx?$/, '')}`) ? 'active' : ''}`} href={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}>
-                                            {/* <a className={`block py-2 px-3 text-sm hover:underline ${isActiveLink(`/posts/${post.filePath.replace(/\.mdx?$/, '')}`) ? 'active' : ''}`}>
-                                                <span className="block text-gray-600 dark:text-gray-400">{post.data.date}</span>
-                                                <span className="block font-semibold">{post.data.title}</span>
-                                            </a> */}   
-                                             <span className={`block py-2 px-3 text-sm hover:underline ${isActiveLink(`/posts/${post.filePath.replace(/\.mdx?$/, '')}`) ? 'active' : ''}`}>
-                                                <span className="block text-gray-600 dark:text-gray-400">{post.data.date}</span>
-                                                <span className="block font-semibold">{post.data.title}</span>
-                                            </span>
-                                        </Link>
-                                        <Links links={post.data.links} />
-                                       { /* eslint-enable  */}
-                                    </li>
-                                ))}
-                            </ul>
-                        </li>
-          
+  // Sort caseStudy posts by the order property
+  const sortedCaseStudy = caseStudy.sort((a, b) => a.data.order - b.data.order);
 
-                    </ul>
-                </nav>
-            </div>
-        </aside>
-    );
-}
+  return (
+    <aside className="main">
+      <div className='sideabar-inner'>
+        <nav>
+          <ul>
+            <li>
+              <h3>Case Study</h3>
+              <span>Real Estate</span>
+              <ul className='articles'>
+                {sortedCaseStudy.map((post) => (
+                  <li key={post.filePath}>
+                    <Link className={`${isActiveLink(`/posts/${post.filePath.replace(/\.mdx?$/, '')}`) ? 'active' : ''}`} href={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}>
+                      <span className={`block py-2 px-3 text-sm hover:underline ${isActiveLink(`/posts/${post.filePath.replace(/\.mdx?$/, '')}`) ? 'active' : ''}`}>
+                        <span className="block text-gray-600 dark:text-gray-400">{post.data.date}</span>
+                        <span className="block font-semibold">{post.data.title}</span>
+                      </span>
+                    </Link>
+                    <Links links={post.data.links} />
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+  );
+};
 
 export default Sidebar;
