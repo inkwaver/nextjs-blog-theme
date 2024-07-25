b1aafa96f2213f9cb2e5d91bf1012712a8c40a96
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-07-25 13:58:54 +0400

    image viewer usage and caption

--- components/ImageViewerModal.js
+++ components/ImageViewerModal.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './ImageViewerModal.module.css';
 
-const ImageViewerModal = ({ src, alt, width, height, isPriority = false }) => {
+const ImageViewerModal = ({ src, alt, caption, width, height, isPriority = false }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [aspectRatio, setAspectRatio] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -29,11 +29,15 @@ const ImageViewerModal = ({ src, alt, width, height, isPriority = false }) => {
     setIsOpen(false);
   };
 
+  const viewSource = () => {
+    window.open(src, '_blank');
+  };
+
   if (loading) return <div>Loading...</div>;
 
   return (
     <>
-      <div 
+      <figure 
         className={styles.thumbnailContainer} 
         style={{ width: width || '100%', paddingTop: aspectRatio ? `${(1 / aspectRatio) * 100}%` : '56.25%' }}
         onClick={openModal}
@@ -47,11 +51,12 @@ const ImageViewerModal = ({ src, alt, width, height, isPriority = false }) => {
           priority={isPriority}
           style={{ objectFit: 'contain' }}
         />
-      </div>
+        {caption && <figcaption className={styles.caption}>{caption}</figcaption>}
+      </figure>
       {isOpen && (
         <div className={styles.modal} onClick={closeModal}>
           <span className={styles.close} onClick={closeModal}>&times;</span>
-          <div className={styles.modalContentWrapper}>
+          <figure className={styles.modalContentWrapper}>
             <Image
               className={styles.modalContent}
               src={src}
@@ -60,7 +65,11 @@ const ImageViewerModal = ({ src, alt, width, height, isPriority = false }) => {
               sizes="100vw"
               style={{ objectFit: 'contain' }}
             />
-          </div>
+            <button className={styles.viewSourceButton} onClick={viewSource}>
+              View Source
+            </button>
+            {caption && <figcaption className={styles.modalCaption}>{caption}</figcaption>}
+          </figure>
         </div>
       )}
     </>
