3d066b29f3449c43578cbd8ffb65973a80d93832
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-07-30 04:05:04 +0400

    next js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 const withMDX = require('@next/mdx')()
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Image optimization configuration using remotePatterns
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'narek-ch.space',
+        pathname: '**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'], // Include AVIF and WebP formats
+    loader: 'default', // Use the default loader
+  },
   // Optionally, add any other Next.js config below
 }
- 
+
 module.exports = withMDX(nextConfig)
