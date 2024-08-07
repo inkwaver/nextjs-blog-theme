4fd1c98703e138dfe942e0e7ad46eba9747ed5b4
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-08-07 14:37:58 +0400

    footer structure

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,47 @@
-
+import { LinkedIn, Behance } from './Icons/DesignFlowIcons';
+import Link from 'next/link';
 
 export default function Footer({ copyrightText }) {
   return (
     <footer className="main py-16 flex flex-col items-center">
-      <p className="dark:text-white uppercase mb-3 font-bold opacity-60">
+   <div className='first-floor wrapper-main'> 
+   <section>
+        <h3>Design</h3>
+        <ul>
+          <li><Link href="">Case Study Real Estate</Link></li>
+          <li><Link href="">Show Case Beargeek</Link></li>
+          <li><Link href="">Show Case Festberg</Link></li>
+        </ul>
+      </section>
+      <section>
+        <h3>Development</h3>
+        <ul>
+          <li><Link href="">Skills</Link></li>
+          <li><Link href="">Projects</Link></li>
+        </ul>
+      </section>
+      <section>
+        <p className="connect">
+          Connect Via
+        <Link className="icon-24" target="_blank" href="https://www.linkedin.com/in/narekchilingaryan/">
+            <LinkedIn />
+          </Link>
+        </p>
+        <Link className="icon-24"  target="_blank" href="https://www.behance.net/narek-ws">
+            <Behance />
+          </Link>
+          <Link
+            href="/narek-ch_wp-dev-web.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button cv-btn"
+          >
+            Download CV
+          </Link>
+      </section>
+   </div>
+      
+      <p className="wrapper-main">
         {copyrightText}
       </p>
     </footer>
