938827fd710feba80ed4aa3253680d6686041d11
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-08-11 20:25:13 +0400

    last test fuck lighthouse

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'narek-ch.space',
-        pathname: '/**',
-      },
-    ],
-    formats: ['image/avif', 'image/webp'], // Ensure AVIF and WebP formats are enabled
-  },
+
 };
 
 module.exports = withMDX(nextConfig);
