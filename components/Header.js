2e93718cf27ea050ee267dd2ffb5d6e7f12eb19d
Narek Chilingaryan <69740747+inkwaver@users.noreply.github.com>
2024-08-11 19:08:21 +0400

    testing score

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image';
 import { useRouter } from 'next/router';
 import { LinkedIn, Behance } from './Icons/DesignFlowIcons';
 import ThemeSwitcher from './ThemeSwitcher.js';
@@ -8,54 +8,42 @@ import { CloseIcon, MenuIcon } from '../components/Icons/DesignFlowIcons';
 
 export default function Header() {
   const router = useRouter();
-  const [avatarSize, setAvatarSize] = useState(240);
   const [isChecked, setIsChecked] = useState(false);
   const [rootUrl, setRootUrl] = useState('');
-
-  // Function to determine if a link is active
-  const isActiveLink = (href) => {
-    return router.pathname === href;
-  };
 
   const handleChange = (event) => {
     setIsChecked(event.target.checked);
   };
 
   useEffect(() => {
-    // Dynamically set the root URL
     if (typeof window !== 'undefined') {
       setRootUrl(window.location.origin);
     }
 
     const handleScroll = () => {
+      const headerElement = document.querySelector('.main');
       const scrollPosition = window.scrollY;
 
-      // Decrease avatar size gradually as user scrolls
-      const newAvatarSize = Math.max(45, 240 - scrollPosition);
-      setAvatarSize(newAvatarSize);
+      if (headerElement) {
+        if (scrollPosition > 50) {
+          headerElement.classList.add('sticked');
+        } else {
+          headerElement.classList.remove('sticked');
+        }
+      }
     };
 
-    // Apply the avatar size change and scroll effect only on the home page
     if (router.pathname === '/') {
-      // Check the initial scroll position
-      handleScroll();
-
-      // Attach scroll event listener
       window.addEventListener('scroll', handleScroll);
 
-      // Cleanup on component unmount
       return () => {
         window.removeEventListener('scroll', handleScroll);
       };
-    } else {
-      // On other pages, set a fixed avatar size and add the 'sticked' class
-      setAvatarSize(45);
     }
-  }, [router.pathname]); // Listen for changes in the route
+  }, [router.pathname]);
 
   return (
-    <header className={`main ${avatarSize <= 45 ? 'sticked' : 'hero-header'}`}>
-      {/* eslint-disable */}
+    <header className={`main ${router.pathname !== '/' ? 'sticked' : ''}`}>
       <input
         checked={isChecked}
         onChange={handleChange}
@@ -72,16 +60,15 @@ export default function Header() {
         <div className="main-inner">
           <h1 className="logo">
             <Link href="/">
-              <span className={isActiveLink('/') ? 'active' : ''}>
+              <span className={router.pathname === '/' ? 'active' : ''}>
+                <div className='avatar-img'></div>
                 <Image
                   className="avatar"
-                  src="/avatar-nch.png"
-                  width={avatarSize}
-                  height={avatarSize}
+                  src="/narek-ch.png"
+                  width={240}  // Fixed width
+                  height={240} // Fixed height
                   alt="Image description"
-                  loading = 'lazy' 
-                  // priority={true}
-                  // loading="eager" // Ensure the avatar image loads immediately
+                  priority={true} // Ensure the avatar image loads immediately
                 />
               </span>
             </Link>
@@ -96,7 +83,6 @@ export default function Header() {
               that's high-resolution, features smooth scrolling, is
               mobile-friendly, and prioritizes accessibility.
             </p>
-            {/* eslint-enable */}
           </div>
         </div>
         <div className="header-links">
@@ -117,7 +103,7 @@ export default function Header() {
           <Link className="icon-24" target="_blank" href="https://www.linkedin.com/in/narekchilingaryan/">
             <LinkedIn />
           </Link>
-          <Link className="icon-24"  target="_blank" href="https://www.behance.net/narek-ws">
+          <Link className="icon-24" target="_blank" href="https://www.behance.net/narek-ws">
             <Behance />
           </Link>
           <Link
